a5b3da367f5720fc6bc66616fb94b5079e2eefad
Chenning4 <cxu7@caltech.edu>
2025-05-11 17:18:34 -0700

    full update, published

---
+++ src/piegy/__version__.py
@@ -0,0 +1,13 @@
+__version__ = '0.1.6'
+
+'''
+version history:
+
+0.1.0: first publishing, May 11, 2025
+0.1.1: fix dependency errors
+0.1.2: fixing module not find error
+0.1.3: restructuring package
+0.1.4, 0.1.5: fix moviepy import issue
+0.1.6: changed name back to 'piegy'
+
+'''
